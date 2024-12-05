552a225fcbdc9063adde72d54b3932bf4f9c24b9
Adnan Abouelela <adnan.abouelela@gmail.com>
2024-12-05 17:11:23 +0100

    added version

--- src/spaceolotl/app.py
+++ src/spaceolotl/app.py
@@ -1,5 +1,6 @@
 from shiny import App
 from spaceolotl.mod.ui import app_ui as _ui
 from spaceolotl.mod.server import server as _server
-
+from spaceolotl import __version__
+print(__version__)
 app = App(_ui, _server)
